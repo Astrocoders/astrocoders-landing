77e58d10fa55e1b96f22b249913add5d8ac2ce23
Guilherme Decampo <guilherme.decampo@gmail.com>
2015-11-14 16:21:41 -0200

    First Commit

---
+++ scripts/animation.js
@@ -0,0 +1,91 @@
+(function(window){
+
+  function when(selector){
+    var el = document.querySelector(selector);
+    var offsetTop = el.offsetTop - 54;
+    var beyondHeight = offsetTop + el.clientHeight;
+
+    var onIn = function(){};
+    var onOut = function(){};
+
+    var hasIn = false;
+    var hasOut = false;
+
+    window.addEventListener('scroll', function(event){
+      var isIn = window.scrollY >= offsetTop && window.scrollY < offsetTop + 50;
+      var isOut = window.scrollY >= beyondHeight;
+      if(isIn && !hasIn){
+        onIn(el);
+        hasOut = false;
+        hasIn = true;
+      } else if(isOut && !hasOut){
+        onOut(el);
+        hasOut = true;
+        hasIn = false;
+      }
+    });
+
+    return {
+      in: function(fn){
+        onIn = fn;
+
+        return this;
+      },
+
+      out: function(fn){
+        onOut = fn;
+
+        return this;
+      }
+    };
+  }
+
+  (function(window, when){
+    if('ontouchstart' in window) return;
+    
+    window.addEventListener('DOMContentLoaded', function(){
+      when('#team-wrapper')
+        .in(function(el){
+          var items = el.querySelectorAll('.flex-grid-item');
+
+          klass(items, 'add', 'active');
+        })
+        .out(function(el){
+          var items = el.querySelectorAll('.flex-grid-item');
+
+          klass(items, 'remove', 'active');
+        });
+
+        when('#services-article')
+          .in(function(el){
+            var items = el.querySelectorAll('.flex-grid-item');
+
+            klass(items, 'add', 'active');
+          })
+          .out(function(el){
+            var items = el.querySelectorAll('.flex-grid-item');
+
+            klass(items, 'remove', 'active');
+          });
+
+        when('#testimonials-wrapper')
+          .in(function(el){
+            var items = el.querySelectorAll('.flex-grid-item');
+
+            klass(items, 'add', 'active');
+          })
+          .out(function(el){
+            var items = el.querySelectorAll('.flex-grid-item');
+
+            klass(items, 'remove', 'active');
+          });
+    });
+
+    function klass(items, action, klass){
+      for (var i = 0; i < items.length; i++) {
+        items[i].classList[action](klass);
+      }
+    }
+  })(window, when);
+
+})(window);
---
+++ scripts/hire-us-video.js
@@ -0,0 +1,41 @@
+(function(){
+  function playOnScroll(){
+    var NEAR_OFFSET = 50;
+    var contactOffset = document
+                        .querySelector('#contact-wrapper')
+                        .offsetTop;
+    var played = false;
+
+    window.addEventListener('scroll', function(event){
+      if(
+          window.scrollY >= ( contactOffset - NEAR_OFFSET ) &&
+          !played
+      ){
+        player.playVideo();
+        player.setLoop(true);
+        played = true;
+      }
+    });
+  }
+
+  function setupPlayer(){
+    player.mute();
+    player.loadVideoById({
+      videoId: 'OnoNITE-CLc',
+      startSeconds: 87,
+      endSeconds: 60,
+      suggestedQuality: 'large'
+    });
+
+    setTimeout(function(){
+      player.pauseVideo();
+    }, 0);
+  }
+
+  window.addEventListener('load', function(){
+    if('ontouchstart' in window) return;
+    
+    setupPlayer();
+    playOnScroll();
+  });
+})();
