a00de89df51a40082e18d53c86046810ecea26ac
Gabriel Rubens <grsabreu@gmail.com>
2015-11-21 13:21:26 -0200

    Build script use last commit message as default

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 CMMT_MSG=$(git log -1 --pretty=%B)
-echo "\033[1;33mWhat have you done in astrocoders landing page?"
-read -p $CMMT_MSG update
+echo "\033[1;33mWhat have you done in astrocoders landing page?\033[0m"
+read $CMMT_MSG
 
 rm -rf ../astrocoders/*.html
 rm -rf ../astrocoders/*/
@@ -11,6 +11,6 @@ mv www/* ../astrocoders/
 rm -rf www
 cd ../astrocoders
 git add --all
-git commit -m "$update"
+git commit -m "$CMMT_MSG"
 echo "\033[1;31mPushing new site to GitHub repo\n\033[0m"
 git push
