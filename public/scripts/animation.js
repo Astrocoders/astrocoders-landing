d6ddd12222c11e30e6ea87df5fa67cd052745f9c
Guilherme Decampo <guilherme.decampo@gmail.com>
2015-11-26 12:05:43 -0200

    Add header back

--- public/scripts/animation.js
+++ public/scripts/animation.js
@@ -1,91 +1,91 @@
-// (function(window){
+(function(window){
 
-//   function when(selector){
-//     var el = document.querySelector(selector);
-//     var offsetTop = el.offsetTop - 54;
-//     var beyondHeight = offsetTop + el.clientHeight;
+  function when(selector){
+    var el = document.querySelector(selector);
+    var offsetTop = el.offsetTop - 54;
+    var beyondHeight = offsetTop + el.clientHeight;
 
-//     var onIn = function(){};
-//     var onOut = function(){};
+    var onIn = function(){};
+    var onOut = function(){};
 
-//     var hasIn = false;
-//     var hasOut = false;
+    var hasIn = false;
+    var hasOut = false;
 
-//     window.addEventListener('scroll', function(event){
-//       var isIn = window.scrollY >= offsetTop && window.scrollY < offsetTop + 50;
-//       var isOut = window.scrollY >= beyondHeight;
-//       if(isIn && !hasIn){
-//         onIn(el);
-//         hasOut = false;
-//         hasIn = true;
-//       } else if(isOut && !hasOut){
-//         onOut(el);
-//         hasOut = true;
-//         hasIn = false;
-//       }
-//     });
+    window.addEventListener('scroll', function(event){
+      var isIn = window.scrollY >= offsetTop && window.scrollY < offsetTop + 50;
+      var isOut = window.scrollY >= beyondHeight;
+      if(isIn && !hasIn){
+        onIn(el);
+        hasOut = false;
+        hasIn = true;
+      } else if(isOut && !hasOut){
+        onOut(el);
+        hasOut = true;
+        hasIn = false;
+      }
+    });
 
-//     return {
-//       in: function(fn){
-//         onIn = fn;
+    return {
+      in: function(fn){
+        onIn = fn;
 
-//         return this;
-//       },
+        return this;
+      },
 
-//       out: function(fn){
-//         onOut = fn;
+      out: function(fn){
+        onOut = fn;
 
-//         return this;
-//       }
-//     };
-//   }
+        return this;
+      }
+    };
+  }
 
-//   (function(window, when){
-//     if('ontouchstart' in window) return;
+  (function(window, when){
+    if('ontouchstart' in window) return;
 
-//     window.addEventListener('DOMContentLoaded', function(){
-//       when('#team-wrapper')
-//         .in(function(el){
-//           var items = el.querySelectorAll('.flex-grid-item');
+    window.addEventListener('DOMContentLoaded', function(){
+      when('#team-wrapper')
+        .in(function(el){
+          var items = el.querySelectorAll('.flex-grid-item');
 
-//           klass(items, 'add', 'active');
-//         })
-//         .out(function(el){
-//           var items = el.querySelectorAll('.flex-grid-item');
+          klass(items, 'add', 'active');
+        })
+        .out(function(el){
+          var items = el.querySelectorAll('.flex-grid-item');
 
-//           klass(items, 'remove', 'active');
-//         });
+          klass(items, 'remove', 'active');
+        });
 
-//         when('#services-article')
-//           .in(function(el){
-//             var items = el.querySelectorAll('.flex-grid-item');
+        when('#services-article')
+          .in(function(el){
+            var items = el.querySelectorAll('.flex-grid-item');
 
-//             klass(items, 'add', 'active');
-//           })
-//           .out(function(el){
-//             var items = el.querySelectorAll('.flex-grid-item');
+            klass(items, 'add', 'active');
+          })
+          .out(function(el){
+            var items = el.querySelectorAll('.flex-grid-item');
 
-//             klass(items, 'remove', 'active');
-//           });
+            klass(items, 'remove', 'active');
+          });
 
-//         when('#testimonials-wrapper')
-//           .in(function(el){
-//             var items = el.querySelectorAll('.flex-grid-item');
+        when('#testimonials-wrapper')
+          .in(function(el){
+            var items = el.querySelectorAll('.flex-grid-item');
 
-//             klass(items, 'add', 'active');
-//           })
-//           .out(function(el){
-//             var items = el.querySelectorAll('.flex-grid-item');
+            klass(items, 'add', 'active');
+          })
+          .out(function(el){
+            var items = el.querySelectorAll('.flex-grid-item');
 
-//             klass(items, 'remove', 'active');
-//           });
-//     });
+            klass(items, 'remove', 'active');
+          });
+    });
 
-//     function klass(items, action, klass){
-//       for (var i = 0; i < items.length; i++) {
-//         items[i].classList[action](klass);
-//       }
-//     }
-//   })(window, when);
+    function klass(items, action, klass){
+      for (var i = 0; i < items.length; i++) {
+        items[i].classList[action](klass);
+      }
+    }
+  })(window, when);
 
-// })(window);
+})(window);
