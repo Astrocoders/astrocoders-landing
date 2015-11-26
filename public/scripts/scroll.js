aa2835a027b9511399c98ac719c527daf55e1b22
Guilherme Decampo <guilherme.decampo@gmail.com>
2015-11-26 11:06:51 -0200

    Remove animations

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -1,28 +1,28 @@
-(function(window){
-  window.addEventListener('DOMContentLoaded', function(){
-    var navLinks = document.querySelector('nav .links');
+// (function(window){
+//   window.addEventListener('DOMContentLoaded', function(){
+//     var navLinks = document.querySelector('nav .links');
 
-    navLinks.addEventListener('click', delegateEventTo('a', function(event){
-      event.preventDefault();
+//     navLinks.addEventListener('click', delegateEventTo('a', function(event){
+//       event.preventDefault();
 
-      var menuHeight = navLinks.clientHeight;
+//       var menuHeight = navLinks.clientHeight;
 
-      goToAnchorEl(event, menuHeight);
-    }));
-  });
+//       goToAnchorEl(event, menuHeight);
+//     }));
+//   });
 
-  function delegateEventTo(child, callback){
-    return function(event){
-      if( event.target && event.target.nodeName == child.toUpperCase()){
-        callback(event);
-      }
-    };
-  }
+//   function delegateEventTo(child, callback){
+//     return function(event){
+//       if( event.target && event.target.nodeName == child.toUpperCase()){
+//         callback(event);
+//       }
+//     };
+//   }
 
-  function goToAnchorEl(event, offset){
-    var anchor = document.querySelector(event.target.getAttribute('href'));
-    var anchorOffset = anchor.offsetTop;
+//   function goToAnchorEl(event, offset){
+//     var anchor = document.querySelector(event.target.getAttribute('href'));
+//     var anchorOffset = anchor.offsetTop;
 
-    window.scrollTo(0, anchorOffset - offset);
-  }
-})(window);
+//     window.scrollTo(0, anchorOffset - offset);
+//   }
+// })(window);
