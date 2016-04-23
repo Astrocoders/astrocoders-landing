9c50a0531a103a215d9c4e7577ba8f9527380b13
Guilherme Decampo <guilherme.decampo@gmail.com>
2016-04-22 23:55:58 -0300

    Change submit form

--- public/scripts/hire-us-submit.js
+++ public/scripts/hire-us-submit.js
@@ -1,8 +1,40 @@
 window.addEventListener('DOMContentLoaded', function(){
   var form = document.querySelector('#hireUsForm');
   var btn = form.querySelector('button');
+  var inputs = arrayFromTagsList(form.getElementsByTagName('input'));
+  var textarea = arrayFromTagsList(form.getElementsByTagName('textarea'));
+  var fields = inputs.concat(textarea);
+  var xmlhttp = new XMLHttpRequest();
+  var postURL = "https://astromail.astrocoders.com/mail/kAGu38vSsJe4i546T";
+  document.domain = 'astrocoders.com';
 
   form.addEventListener('submit', function(){
+    event.preventDefault();
+    var data = fields.reduce(function(acc, field) {
+      acc[field.name] = field.value;
+      return acc;
+    }, {});
+
+    xmlhttp.send(data);
     btn.disabled = true;
   });
+
+
+  xmlhttp.open("POST", postURL);
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+      if(xmlhttp.status == 200){
+        console.log('Response: ' + xmlhttp.responseText );
+      } else {
+        console.log('Error: ' + xmlhttp.toString() )
+      }
+    }
+  };
+
+  // Helpers
+
+  function arrayFromTagsList(list) {
+    return Array.prototype.slice.call(list);
+  }
+
 });
