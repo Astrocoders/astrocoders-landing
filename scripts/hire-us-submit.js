cf4c15fe146b3ace9d5c713cf24cbfdf3bb2989a
Guilherme Decampo <guilherme.decampo@gmail.com>
2016-04-23 00:40:11 -0300

    Keep with form-urlencoded

--- scripts/hire-us-submit.js
+++ scripts/hire-us-submit.js
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
 
   xmlhttp.open("POST", postURL);
-  xmlhttp.setRequestHeader('Content-type', 'text/plain');
+  xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == XMLHttpRequest.DONE) {
       if(xmlhttp.status == 200){
