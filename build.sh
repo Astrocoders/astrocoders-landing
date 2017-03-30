30d22ccf552c17d372ee988219732ace58ed9480
Gabriel Rubens <grsabreu@gmail.com>
2017-03-30 11:19:12 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,27 +1,34 @@
 #!/bin/sh
 
-echo "\033[1;33m What have you done to the site?"
+TMP_DIR=$(mktemp -d)
+
+echo "\033[1;33m What have you done to the site?\033[0m"
 read update
 
+# Create bundle
 npm run build
 git commit -am "Create new bundle"
 
-TMP_DIR=/tmp/astrocoders-landing
-mkdir $TMP_DIR
-cp -r ./build/** $TMP_DIR
-buildFiles=`ls ./build`
+# Copy build to temp folder
+cp ./build/ $TMP_DIR/build -r
+BUILD_FILES=`ls ./build`
 
+# Wipe out current site
 git checkout master
-
 git rm -rf .
-mv -f $TMP_DIR/** ./
+
+# Move new bundle to master
+mv $TMP_DIR/build/** ./ -f
+
+# Create CNAME
 echo "astrocoders.com" > CNAME
 git add CNAME
-git add $buildFiles
-git commit -m "$update"
+git add $BUILD_FILES
 
+# Commit and push changes
+git commit -m "$update"
 echo "\033[1;31mPushing new site to GitHub repo\n\033[0m"
 git push origin master --force
-git checkout develop
 
-rm -rf $TMP_DIR
+# Get back to develop
+git checkout develop
