0db8a3d73e0fc53458435727111159def39e62db
eliabejr <eliabejunior@hotmail.com.br>
2018-09-19 12:57:02 -0300

    Fix: navbar behavior on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,12 @@ import { FormattedMessage } from 'react-intl'
 import styled from 'styled-components'
 import { compose, withStateHandlers } from 'recompose'
 import ClickOutside from 'react-click-outside'
-import Link from './Link'
-
-import navLogo from '../img/navLogo.svg'
 
 import withNavbar from '../utils/withNavbar'
+import navLogo from '../img/navLogo.svg'
+import Link from './Link'
+
+import theme from '../utils/theme'
 
 const HeaderWrapper = styled.div`
   width: 100%;
@@ -43,23 +44,31 @@ const HeaderLink = styled(Link)`
   font-size: ${props => props.fontSize || '1rem'};
   text-decoration: none;
   margin: 10px;
-  color: #fff;
+  color: ${props => props.color || '#fff'};
   transition: 0.25s;
+
+  &:hover {
+    color: ${theme.colors.primary};
+    @media (max-width: 960px) {
+      color: #fff;
+    }
+  }
 `
 
 const MenuDialog = styled.div`
+  align-items: center;
+  background-color: #000;
+  border-bottom: 2px solid ${theme.colors.primary};
+  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
-  width: 100%;
-  position: fixed;
-  top: 100px;
-  right: 0;
   padding-bottom: 20px;
   padding-top: 20px;
-  align-items: center;
-  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
+  position: fixed;
+  right: 0;
+  top: 100px;
+  width: 100%;
   z-index: 2;
-  background-color: #fff;
 `
 
 const MobileMenu = styled.div`
@@ -70,17 +79,20 @@ const MobileMenu = styled.div`
 `
 
 const MenuTrigger = styled.button`
-  padding-top: 13px;
+  -webkit-appearance: none;
+  background: none;
+  border: none;
+  cursor: pointer;
 `
 
 const MenuIcon = () => (
-  <svg fill="#50c1ce" width="36" height="36" viewBox="0 0 24 24">
+  <svg fill={theme.colors.primary} width="36" height="36" viewBox="0 0 24 24">
     <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
   </svg>
 )
 
 const MenuIconClose = () => (
-  <svg fill="#50c1ce" width="36" height="36" viewBox="0 0 24 24">
+  <svg fill={theme.colors.primary} width="36" height="36" viewBox="0 0 24 24">
     <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
     <path d="M0 0h24v24H0z" fill="none" />
   </svg>
@@ -109,15 +121,11 @@ const Navbar = ({ isHome, isShowed, setMenuOpened, isMenuOpened }) => (
           </HeaderContainer>
           {isMenuOpened && (
             <MenuDialog>
-              <HeaderLink color="#14646d" fontSize="1.5rem" to="/#projects" onClick={evt => setMenuOpened()}>
-                <FormattedMessage id="about" />
-              </HeaderLink>
-              <HeaderLink color="#14646d" fontSize="1.5rem" to="/about" onClick={evt => setMenuOpened()}>
-                <FormattedMessage id="features" />
-              </HeaderLink>
-              <HeaderLink color="#14646d" fontSize="1.5rem" to="/#contact" onClick={evt => setMenuOpened()}>
-                <FormattedMessage id="contact" />
-              </HeaderLink>
+              <HeaderLink to="#services-article">HOW WE WORK</HeaderLink>
+              <HeaderLink to="#team-wrapper">OPEN SOURCE</HeaderLink>
+              <HeaderLink to="#testimonials-wrapper">JOIN US</HeaderLink>
+              <HeaderLink to="/cases">CASES</HeaderLink>
+              <HeaderLink to="#contact-wrapper">CONTACT</HeaderLink>
             </MenuDialog>
           )}
         </MobileMenu>
