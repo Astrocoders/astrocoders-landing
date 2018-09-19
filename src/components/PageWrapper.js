9a6843f6e9638e8f0e7353dfa0820ca1bbc8f706
eliabejr <eliabejunior@hotmail.com.br>
2018-09-19 13:56:16 -0300

    Fix: capitalize Sections folder name

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { injectIntl } from 'react-intl'
 
 import Navbar from './Navbar'
-import Footer from '../sections/Footer'
-import HireUs from '../sections/HireUs'
+import Footer from '../Sections/Footer'
+import HireUs from '../Sections/HireUs'
 
 import data from '../data.json'
 
