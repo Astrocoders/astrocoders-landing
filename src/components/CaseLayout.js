8aa190b4ffef5b9cbff8e57d359ea2bfa39f9e73
eliabejr <eliabejunior@hotmail.com.br>
2019-01-17 16:14:52 -0300

    feat(revamp-cases): add call to action inside cases pages

--- src/components/CaseLayout.js
+++ src/components/CaseLayout.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import TextLoop from 'react-text-loop'
 
 import PageWrapper from './PageWrapper'
 import Title from './Title'
 import Wrapper from './Wrapper'
+
+import theme from '../utils/theme'
 
 const ProjectCover = styled.div`
   align-items: center;
@@ -19,18 +23,71 @@ const ProjectImage = styled.img`
   width: 150px;
 `
 
-const CaseLayout = ({ bgColor, cover, title, titleColor, content }) => (
+const ProjectTitle = styled(Title)`
+  background-color: #fff;
+  color: ${props => props.color};
+  font-family: ${theme.fonts.caseTitle};
+  font-size: 3em;
+  padding: 20px;
+  margin-bottom: -120px;
+`
+
+const ProjectContent = styled(Wrapper)`
+  h3 {
+    &:after {
+      content: '';
+      display: block;
+      background: ${props => props.color};
+      height: 4px;
+      margin-top: 10px;
+      width: 30px;
+    }
+  }
+`
+
+const CTA = styled(Title)`
+  font-family: ${theme.fonts.caseTitle};
+
+  span {
+    padding-left: 10px;
+    padding-right: 10px;
+    color: ${theme.colors.primary};
+    font-weight: 700;
+  }
+`
+
+const CaseLayout = ({ color, logo, title, content }) => (
   <PageWrapper isHome={false}>
-    <ProjectCover bgColor={bgColor}>
-      <ProjectImage src={cover} />
-      <Title color={titleColor || '#000'} weight="700" align="center">
-        {title}
-      </Title>
+    <ProjectCover bgColor={color}>
+      <ProjectImage src={logo} />
+      <ProjectTitle color={color}>{title}</ProjectTitle>
     </ProjectCover>
-    <Wrapper marginTop="150px" marginBottom="150px" alignItems="">
+    <ProjectContent marginTop="150px" color={color}>
       {content}
-    </Wrapper>
+      <CTA marginTop="100px">
+        Ready to
+        <TextLoop interval={2000} mask={true}>
+          <span>start</span>
+          <span>continue</span>
+          <span>improve</span>
+        </TextLoop>
+        your business? Get in touch in the below!
+      </CTA>
+    </ProjectContent>
   </PageWrapper>
 )
+
+CaseLayout.propTypes = {
+  content: PropTypes.node.isRequired,
+  color: PropTypes.string.isRequired,
+  logo: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+}
+
+CaseLayout.defaultProps = {
+  color: theme.colors.primary,
+  logo: '',
+  title: '',
+}
 
 export default CaseLayout
