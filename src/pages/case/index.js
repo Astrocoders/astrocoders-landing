db2a45e348a851661d08bb635b1a67ffa9866caa
eliabejr <eliabejunior@hotmail.com.br>
2019-05-24 13:31:59 -0300

    chore: add redirect on folders root

---
+++ src/pages/case/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+const NotFoundPage = () => (
+  <Helmet>
+    <meta http-equiv="refresh" content="1;URL=/404" />
+  </Helmet>
+)
+
+export default NotFoundPage
