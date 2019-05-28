0eeb218f6954971f11e6bd65ee24fd165e326c87
eliabejr <eliabejunior@hotmail.com.br>
2019-05-28 02:39:46 -0300

    feat(products-page): add ProductTestimonial and Features on Products Page

--- src/pages/product/astral.js
+++ src/pages/product/astral.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FormattedMessage, injectIntl } from 'gatsby-plugin-intl'
 
 import ProductLayout from '../../components/ProductLayout'
 import Button from '../../components/Button'
 import Title from '../../components/Title'
 import Text from '../../components/Text'
+import ProductTestimonial from '../../components/ProductTestimonial'
 import HGroup from '../../components/HGroup'
 import VGroup from '../../components/VGroup'
+import Feature from '../../components/Feature'
 
 import astroGreen from '../../img/astroGreen.png'
 import astralPreview from '../../img/products/astralPreview.png'
 
-import theme from '../../utils/theme'
+import pamPhoto from '../../img/testimonials/pam.jpg'
+import zuraPhoto from '../../img/testimonials/ky-js.jpg'
 
-const Link = styled.a`
-  color: ${props => props.color};
-  text-decoration: none;
-`
+import theme from '../../utils/theme'
 
 const Preview = styled.div`
   background-color: #262a41;
@@ -43,7 +44,30 @@ const Image = styled.img`
   transition: all 0.2s ease-in-out;
 `
 
-const AstralPanel = () => (
+const TestimonialsWrapper = styled.section`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: 80px 0;
+  width: 100%;
+`
+
+const TestimonialsList = styled(HGroup)`
+  margin-top: 50px;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`
+
+const Features = styled.section`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: 80px 0;
+  width: 100%;
+`
+
+const AstralPanel = ({ intl }) => (
   <ProductLayout
     logo={astroGreen}
     title="Astral Panel"
@@ -70,9 +94,45 @@ const AstralPanel = () => (
             <Button theme={theme.colors.astrogreen}>Try It</Button>
           </VGroup>
         </HGroup>
+
+        <Features>
+          <Title align="center" transform="uppercase" withStripe>
+            What is in the package?
+          </Title>
+
+          <HGroup>
+            <Feature name="Easy to use" icon="https://image.flaticon.com/icons/svg/1642/1642303.svg" />
+            <Feature name="Complete Control" icon="https://image.flaticon.com/icons/svg/1642/1642284.svg" />
+            <Feature name="Fully Scalable" icon="https://image.flaticon.com/icons/svg/1642/1642314.svg" />
+          </HGroup>
+        </Features>
+
+        <TestimonialsWrapper>
+          <Title align="center" transform="uppercase" withStripe>
+            What our partners built with us
+          </Title>
+          <TestimonialsList>
+            <ProductTestimonial
+              key="https://lion.tax/"
+              name="Pamela Borges"
+              company="Lion Tax"
+              quote={intl.formatMessage({ id: 'quote1' })}
+              link="https://lion.tax/"
+              photo={pamPhoto}
+            />
+            <ProductTestimonial
+              key="https://zurahealth.com/"
+              name="Kylie and Jess McBeath"
+              company="Zura Health"
+              quote={intl.formatMessage({ id: 'quote2' })}
+              link="https://zurahealth.com/"
+              photo={zuraPhoto}
+            />
+          </TestimonialsList>
+        </TestimonialsWrapper>
       </>
     }
   />
 )
 
-export default AstralPanel
+export default injectIntl(AstralPanel)
--- src/pages/product/astrocontrole.js
+++ src/pages/product/astrocontrole.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FormattedMessage, injectIntl } from 'gatsby-plugin-intl'
 
 import ProductLayout from '../../components/ProductLayout'
 import Button from '../../components/Button'
 import Title from '../../components/Title'
 import Text from '../../components/Text'
-import Testimonials from '../../components/Testimonials'
+import ProductTestimonial from '../../components/ProductTestimonial'
 import HGroup from '../../components/HGroup'
 import VGroup from '../../components/VGroup'
+import Feature from '../../components/Feature'
 
 import astroGreen from '../../img/astroGreen.png'
 import astrocontrolePreview from '../../img/products/astrocontrolePreview.png'
 
-import theme from '../../utils/theme'
+import pamPhoto from '../../img/testimonials/pam.jpg'
+import zuraPhoto from '../../img/testimonials/ky-js.jpg'
 
-const Link = styled.a`
-  color: ${props => props.color};
-  text-decoration: none;
-`
+import theme from '../../utils/theme'
 
 const WhatIsThis = styled(HGroup)`
   align-items: center;
@@ -57,7 +57,30 @@ const Image = styled.img`
   transition: all 2s ease-in-out;
 `
 
-const Astrocontrole = () => (
+const TestimonialsWrapper = styled.section`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: 80px 0;
+  width: 100%;
+`
+
+const TestimonialsList = styled(HGroup)`
+  margin-top: 50px;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`
+
+const Features = styled.section`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: 80px 0;
+  width: 100%;
+`
+
+const Astrocontrole = ({ intl }) => (
   <ProductLayout
     logo={astroGreen}
     title="Astrocontrole"
@@ -72,7 +95,7 @@ const Astrocontrole = () => (
 
         <WhatIsThis columnOnMobile>
           <VGroup>
-            <Title>What is this?</Title>
+            <Title transform="uppercase">What is this?</Title>
             <Text marginTop="20px" marginBottom="20px">
               We developed a calendar system for expatriates to control their time stayed on countries. Allowing a
               better management that mitigate tax/fees/fines due to extent time.
@@ -85,10 +108,44 @@ const Astrocontrole = () => (
           </Preview>
         </WhatIsThis>
 
-        <Testimonials />
+        <Features>
+          <Title align="center" transform="uppercase" withStripe>
+            What is in the package?
+          </Title>
+
+          <HGroup>
+            <Feature name="Easy to use" icon="https://image.flaticon.com/icons/svg/1642/1642303.svg" />
+            <Feature name="Complete Control" icon="https://image.flaticon.com/icons/svg/1642/1642284.svg" />
+            <Feature name="Fully Scalable" icon="https://image.flaticon.com/icons/svg/1642/1642314.svg" />
+          </HGroup>
+        </Features>
+
+        <TestimonialsWrapper>
+          <Title align="center" transform="uppercase" withStripe>
+            What our partners built with us
+          </Title>
+          <TestimonialsList>
+            <ProductTestimonial
+              key="https://lion.tax/"
+              name="Pamela Borges"
+              company="Lion Tax"
+              quote={intl.formatMessage({ id: 'quote1' })}
+              link="https://lion.tax/"
+              photo={pamPhoto}
+            />
+            <ProductTestimonial
+              key="https://zurahealth.com/"
+              name="Kylie and Jess McBeath"
+              company="Zura Health"
+              quote={intl.formatMessage({ id: 'quote2' })}
+              link="https://zurahealth.com/"
+              photo={zuraPhoto}
+            />
+          </TestimonialsList>
+        </TestimonialsWrapper>
       </>
     }
   />
 )
 
-export default Astrocontrole
+export default injectIntl(Astrocontrole)
